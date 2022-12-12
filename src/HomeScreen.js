6a418dfc462115fde9b8b9326ed68d06bd4547c1
sorinupwork <sorin.upwork@gmail.com>
2022-12-12 19:41:17 +0200

    add row information

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,7 +1,10 @@
 import React from "react";
+
 import Banner from "./Banner";
 import "./HomeScreen.css";
 import Nav from "./Nav";
+import requests from "./requests";
+import Row from "./Row";
 
 function HomeScreen() {
   return (
@@ -10,7 +13,18 @@ function HomeScreen() {
 
       <Banner />
 
-      {/* Row */}
+      <Row
+        title={"NETFLIX ORIGINALS"}
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title={"Trending Now"} fetchUrl={requests.fetchTrending} />
+      <Row title={"Top Rated"} fetchUrl={requests.fetchTopRated} />
+      <Row title={"Action Movies"} fetchUrl={requests.fetchActionMovies} />
+      <Row title={"Comedy Movies"} fetchUrl={requests.fetchComedyMovies} />
+      <Row title={"Horrow Movies"} fetchUrl={requests.fetchHorrorMovies} />
+      <Row title={"Romance Movies"} fetchUrl={requests.fetchRomanceMovies} />
+      <Row title={"Documentaries"} fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
