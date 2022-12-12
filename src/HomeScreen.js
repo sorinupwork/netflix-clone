a42eb5637a1b1b7ec835001e6c7e612e759d5ca1
sorinupwork <sorin.upwork@gmail.com>
2022-12-12 19:01:36 +0200

    add the banner component and fetch data from tmdb

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,14 +1,14 @@
 import React from "react";
+import Banner from "./Banner";
 import "./HomeScreen.css";
 import Nav from "./Nav";
 
 function HomeScreen() {
   return (
     <div className="homeScreen">
-      {/* Nav */}
       <Nav />
 
-      {/* Banner */}
+      <Banner />
 
       {/* Row */}
     </div>
