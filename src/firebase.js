3f3916ac52b126ea339b6d943089a77e4db9e29a
sorinupwork <sorin.upwork@gmail.com>
2022-12-12 14:49:38 +0200

    build up navbar for netflix

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBgIUm9BW2o9ANL5OWpoObm6ZR_fWRkzXE",
+  authDomain: "netflix-clone-a9c50.firebaseapp.com",
+  projectId: "netflix-clone-a9c50",
+  storageBucket: "netflix-clone-a9c50.appspot.com",
+  messagingSenderId: "778161856364",
+  appId: "1:778161856364:web:e72ea76fc8f242e0189d49",
+};
